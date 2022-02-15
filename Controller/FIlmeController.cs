c2218edc0fc252407ea81dfd3d50a107fc06f607
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2022-02-15 13:58:42 -0300

    O esqueleto está oficialmente pronto

--- Controller/FIlmeController.cs
+++ Controller/FIlmeController.cs
@@ -9,7 +9,31 @@ namespace ProjetinhoComExceptionsCSharp.Controller
 {
     public class FilmeController
     {
+        public Filme CriaFilme(string nome, int duracao, string sinopse, int notaDoFilmasso, Categoria categoria)
+        {
 
+            return new Filme(nome,duracao, sinopse,notaDoFilmasso, categoria);
+        }
+
+        public Filme CriaFilme(string nome, int duracao, string sinopse, int notaDoFilmasso, string nomeCategoria)
+        {
+            CategoriaController categoriaController = new CategoriaController();
+            Categoria categoria;
+            categoria = categoriaController.CriaCategoria(nomeCategoria);
+            return new Filme(nome, duracao, sinopse, notaDoFilmasso, categoria);
+        }
+
+        public Categoria AdicionaCategoria(Categoria categoria, Filme filme)
+        {
+            filme.Categorias.Add(categoria);
+            return categoria;
+        }
+
+        public Filme EditarFilme(Filme filme, Filme novoFilme)
+        {
+            filme = novoFilme;
+            return filme;
+        }
 
         public Categoria EditaCategoria(Categoria categoria, string nome)
         {
