a0471c4a49f331c51b0164a54f0381685a63f7c8
ImmortalLotus <72933183+ImmortalLotus@users.noreply.github.com>
2022-02-15 14:36:43 -0300

    Muitas exceptions adicionadas

---
+++ Exceptions/NomeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetinhoComExceptionsCSharp.Exceptions
+{
+    public class NomeException : Exception
+    {
+        public NomeException(){}
+        public NomeException(string mensagem) : base(mensagem){}
+    }
+}
---
+++ Exceptions/SinopseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetinhoComExceptionsCSharp.Exceptions
+{
+    public class SinopseException : ArgumentException
+    {
+        public SinopseException(){}
+        public SinopseException(string mensagem) : base(mensagem){}
+    }
+}
